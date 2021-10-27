aa27da4f23d35dee07ddbbae441e3ebb3afe8bea
Sofia Luna <Sofia Luna@LAPTOP-KDK0IDS2>
2021-10-26 19:49:17 -0700

    Agregar archivos de proyecto.

---
+++ Practica_MVC_4/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practica_MVC_4.Models;
+
+namespace Practica_MVC_4.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FormularioVisita()
+        {
+            return View();
+        }
+
+        public ActionResult CargaDatos()
+        {
+            string nombre = Request.Form["nombre"].ToString();
+            string comentarios = Request.Form["comentarios"].ToString();
+            LibroVisitas libro = new LibroVisitas();
+            libro.Grabar(nombre, comentarios);
+            return View();
+        }
+    }
+}
